e07f22f115f48fae772d0bdeaa255e2f77d7184b
AntonLitovka <antlitovka@gmail.com>
2016-03-08 00:04:18 +0200

    2

---
+++ hello.py
@@ -0,0 +1,8 @@
+
+
+def app(environ, start_response):
+    status = '200 OK'
+    headers = [ ('Content-type', 'text/plain')]
+    body = environ['QUERY_STRING'].replace('&', '\n')
+    start_response(status, headers)
+    return [body]
