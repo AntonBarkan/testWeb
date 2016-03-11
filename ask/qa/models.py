bae346a273503e87c3394a92c23ba42728908f78
AntonLitovka <antlitovka@gmail.com>
2016-03-12 00:08:07 +0200

    m

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+	title = models.CharField(max_length=250)
+	text = models.CharField(max_length=1000)
+	addet_at = models.DateTimeField()
+	rating = models.IntegerField(default=0)
+	author = models..ForeignKey(models.User)
+	likes = models.IntegerField(default=0)
+
+class Answer(models.Model):
+	text = models.CharField(max_length=1000)
+	addet_at = models.DateTimeField()
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	author= models..ForeignKey(models.User)
---
+++ ask/run.sh
@@ -0,0 +1,1 @@
+sudo gunicorn ask.wsgi:application --bind 127.0.0.1:8000
